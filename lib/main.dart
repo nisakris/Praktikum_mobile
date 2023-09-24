d12f85ddd1ffd25739d1b68485fe3ea35fb9c0ec
nisakris <137410285+nisakris@users.noreply.github.com>
2023-09-24 12:24:04 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,64 @@
 import 'package:flutter/material.dart';
+import 'profile.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          backgroundColor: Color(0xdfacd5f6),
+          appBar: AppBar(
+            backgroundColor: Colors.blueAccent,
+            iconTheme: IconThemeData(color: Colors.black),
+            leading: Icon(Icons.home, size: 30),
+            title: Text(
+              "Project Uji Coba",
+              style: TextStyle(color: Colors.white),
+            ),
+            centerTitle: true,
+            titleSpacing: 0,
+            actions: const [
+              Icon(Icons.menu, size: 30),
+            ],
+          ),
+          body: ListView.builder(
+            itemCount: Profile.nama.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.only(
+                  top: 20,
+                ),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    foregroundImage: NetworkImage(
+                        "https://lh3.googleusercontent.com/proxy/esjjzRYoXlhgNYXqU8Gf_3lu6V-eONTnymkLzdwQ6F6z0MWAqIwIpqgq_lk4caRIZF_0Uqb5U8NWNrJcaeTuCjp7xZlpL48JDx-qzAXSTh00AVVqBoT7MJ0259pik9mnQ1LldFLfHZUGDGY=w1200-h630-p-k-no-nu"),
+                  ),
+                  title: Text(
+                    '${Profile.nama[index]}',
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  subtitle: Text(
+                    'Jabatan : ${Profile.jabatan[index]}',
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+              );
+            },
+          )),
     );
   }
 }
