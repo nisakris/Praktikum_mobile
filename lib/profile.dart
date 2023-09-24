451ca30708c820adf68edd0d5ee968fcbadd9c31
nisakris <137410285+nisakris@users.noreply.github.com>
2023-09-24 12:27:54 +0700

    Create profile.dart

---
+++ lib/profile.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Profile {
+  static const List<String> nama = <String>[
+    'Nisa Kris', 'Nisa Kris', 'Nisa Cahyani', 'Nisa Kris',
+    'Nisa Kris', 'Nisa Kris', 'Nisa Kris', 'Nisa Kris',
+    'Nisa Kris', 'Nisa Kris', 'Nisa Kris', 'Nisa Kris',
+    'Nisa Kris', 'Nisa Kris', 'Nisa Kris', 'Nisa Kris',
+    'Nisa Kris', 'Nisa Kris', 'Nisa Kris', 'Nisa Kris'
+  ];
+  static const List<String> jabatan = <String>[
+    'Mahasiswa', 'Mahasiswa', 'Mahasiswa', 'Mahasiswa',
+    'Mahasiswa', 'Mahasiswa', 'Mahasiswa', 'Mahasiswa',
+    'Mahasiswa', 'Mahasiswa', 'Mahasiswa', 'Mahasiswa',
+    'Mahasiswa', 'Mahasiswa', 'Mahasiswa', 'Mahasiswa',
+    'Mahasiswa', 'Mahasiswa', 'Mahasiswa', 'Mahasiswa'
+  ];
+}
